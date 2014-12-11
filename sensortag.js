d0f2da1665c9e15f6e94d59046a2aa8c610a3c38
Jussi Löf <jussilof@gmail.com>
2014-12-12 00:23:50 +0100

    calculations

--- sensortag.js
+++ sensortag.js
@@ -12,7 +12,7 @@ var TIBase = ['f000', '04514000b000000000000000'],
         "2A05": ['Service Changed'],
         "2A23": ['System ID'],
         "2A24": ['Model Number String', '4E:2E:41:2E:00'],
-        "2A25": ['Serial Number String','4E:2E:41:2E:00 '],
+        "2A25": ['Serial Number String', '4E:2E:41:2E:00 '],
         "2A26": ['Firmware Revision String'],
         "2A27": ['Hardware Revision String'],
         "2A28": ['Software Revision String'],
@@ -50,53 +50,45 @@ var TIBase = ['f000', '04514000b000000000000000'],
         AA51: ['Gyroscope Data', 'XLSB:XMSB:YLSB:YMSB:ZLSB:ZMSB'],
         AA52: ['Gyroscope Config', 'Write 0 to turn off gyroscope,1 to enable X-axis only, 2t o enable Y-axis only, 3=X and Y,4 = Zo nly, 5 = X and Z, 6 = Y'],
         AA53: ['Gyroscope Period', 'Period =[Input*10]ms(lower limit100ms),default 1000ms'],
-    
+
         AA61: ['TestData', 'Self-test results (highbitindicatesPASSED):Bit 0:IRtemp,1:Humidity,2:Magnetometer,3:accelerometer,4:Barometer,5:Gyroscope'],
-    
+
         AA62: ['Test Config', 'bit 7: enable testmode; bit 0-1LED bit mask'],
-    
+
         CCC1: ['Connection Parameters', 'ConnInterval,SlaveLatency,SupervisionTimeout(2 byteseach)'],
-    
+
         CCC2: ['Request Connection Parameters', 'MinConnInterval,MaxConnInterval,SlaveLatency,SupervisionTimeout(2 byteseach)'],
-    
+
         CCC3: ['Disconnect request', 'Change thevaluetodisconnect'],
-    
+
         FFC1: ['OADImage Identify', 'Write "0" to identify image type "A", "1" to identify "B". Data in notification 8 bytes: imagetype(2), size/4(2),userdata(4).'],
-    
+
         FFC2: ['OADImage Block', 'Imageblock(18bytes).Blockno. (2 bytes),OADimage block (16 bytes)'],
 
         FFE0: ['Keypress service'],
+
         FFE1: ['Keypress Data'],
+
         "2902": ['Keypress Notification']
 
     };
 
 
 function getTiAttribute(uuid) {
-
     var id = uuid.replace(TIBase[0], '').replace(TIBase[1], '').toUpperCase();
-
     return [uuid, id].concat(sensorTagAttributeTable[id]);
-
 }
 
 function MSBLSBtoWORD(msb, lsb) {
-
     return (((msb << 8) | lsb) << 16) >> 16;
 }
 
 function SensorTag(p, cb) {
-
     this.p = p;
     this.rssi = 0;
-
-    console.log('FOUND->',this.p.advertisement);
-
+    console.log('FOUND->', this.p.advertisement);
     this.init(p, cb);
-
 }
-
-
 
 
 SensorTag.prototype = {
@@ -162,6 +154,19 @@ SensorTag.prototype = {
 
     },
 
+    readConfig: function(cname, cb) {
+        var st = this;
+        st[cname].ch.read(function(err, buf) {
+            cb(st, st[cname].info[2], err, buf);
+        });
+    },
+    readConfigString: function(cname, cb) {
+        var st = this;
+        st[cname].ch.read(function(err, buf) {
+            cb(st, st[cname].info[2], err, buf.toString());
+        });
+    },
+
     readConfigHex: function(cname, cb) {
         var st = this;
         st[cname].ch.read(function(err, buf) {
@@ -170,25 +175,39 @@ SensorTag.prototype = {
     },
 
     getSystemId: function(cb) {
-        this.readConfigHex('SystemID', cb);
+        var st = this;
+        st.SystemID.ch.read(function(err, data) {
+
+            var systemId = [
+                data.readUInt8(7).toString(16),
+                data.readUInt8(6).toString(16),
+                data.readUInt8(5).toString(16),
+                data.readUInt8(4).toString(16),
+                data.readUInt8(3).toString(16),
+                data.readUInt8(2).toString(16),
+                data.readUInt8(2).toString(16),
+                data.readUInt8(0).toString(16)
+            ].join(':');
+            cb(st, st.SystemID.info[2], err, systemId);
+        });
     },
 
     getModelNumberString: function(cb) {
-        this.readConfigHex('ModelNumberString', cb);
+        this.readConfigString('ModelNumberString', cb);
     },
 
     getSerialNumberString: function(cb) {
-        this.readConfigHex('SerialNumberString', cb);
+        this.readConfigString('SerialNumberString', cb);
     },
 
     getFirmwareRevisionString: function(cb) {
-        this.readConfigHex('FirmwareRevisionString', cb);
+        this.readConfigString('FirmwareRevisionString', cb);
     },
 
     ////////////////////////////////////////////////////////////////////////////////////
 
     getIRTemperatureConfig: function(cb) {
-        this.readConfigHex('IRTemperatureConfig', cb);
+        this.readConfig('IRTemperatureConfig', cb);
     },
 
     startIRTemperature: function(cb) {
@@ -206,34 +225,27 @@ SensorTag.prototype = {
 
         st.IRTemperatureData.ch.read(function(err, data) {
 
-            var objT = MSBLSBtoWORD(data[1], data[0]),
-                ambT = MSBLSBtoWORD(data[3], data[2]),
-                m_tmpAmb = ambT / 128.0,
-                Vobj2 = objT * 0.00000015625,
-                Tdie2 = m_tmpAmb + 273.15,
-                S0 = 6.4E-14, // Calibration factor
-                a1 = 1.75E-3,
-                a2 = -1.678E-5,
-                b0 = -2.94E-5,
-                b1 = -5.7E-7,
-                b2 = 4.63E-9,
+            var ambientTemperature = data.readInt16LE(2) / 128.0,
+                Vobj2 = data.readInt16LE(0) * 0.00000015625,
+                Tdie2 = ambientTemperature + 273.15,
+                S0 = 5.593 * Math.pow(10, -14),
+                a1 = 1.75 * Math.pow(10, -3),
+                a2 = -1.678 * Math.pow(10, -5),
+                b0 = -2.94 * Math.pow(10, -5),
+                b1 = -5.7 * Math.pow(10, -7),
+                b2 = 4.63 * Math.pow(10, -9),
                 c2 = 13.4,
                 Tref = 298.15,
                 S = S0 * (1 + a1 * (Tdie2 - Tref) + a2 * Math.pow((Tdie2 - Tref), 2)),
                 Vos = b0 + b1 * (Tdie2 - Tref) + b2 * Math.pow((Tdie2 - Tref), 2),
                 fObj = (Vobj2 - Vos) + c2 * Math.pow((Vobj2 - Vos), 2),
-                tObj = Math.pow(Math.pow(Tdie2, 4) + (fObj / S), 0.25);
+                objectTemperature = Math.pow(Math.pow(Tdie2, 4) + (fObj / S), 0.25);
+            objectTemperature = (objectTemperature - 273.15);
 
-            tObj = (tObj - 273.15);
-
-            var TObj = {
-                rawData : data,
-                objTempWORD: objT,
-                ambTempWORD: ambT,
-                objectTemp : tObj
-            };
-
-            cb(st, st.IRTemperatureData.info[2], err, TObj);
+            cb(st, st.IRTemperatureData.info[2], err, {
+                objectTemperature: objectTemperature,
+                ambientTemperature: ambientTemperature
+            });
 
         });
     },
@@ -259,16 +271,13 @@ SensorTag.prototype = {
 
         st.HumidityData.ch.read(function(err, data) {
 
-            var TempWORD = MSBLSBtoWORD(data[1], data[0]),
-                HumidityWORD = MSBLSBtoWORD(data[3], data[2]);
+            var temperature = -46.85 + 175.72 / 65536.0 * data.readUInt16LE(0),
+                humidity = -6.0 + 125.0 / 65536.0 * (data.readUInt16LE(2) & ~0x0003);
 
-            var HObj = {
-                rawData: data,
-                tempWORD: TempWORD,
-                humidityWORD: HumidityWORD
-            };
-
-            cb(st, st.HumidityData.info[2], err, HObj);
+            cb(st, st.HumidityData.info[2], err, {
+                temperature: temperature,
+                humidity: humidity
+            });
 
 
         });
@@ -295,11 +304,8 @@ SensorTag.prototype = {
             st = this;
         this.BarometerConfig.ch.write(calibrationBit, true, function() {
             st.BarometerCalibration.ch.read(function(err, data) {
-
-                var CObj = {
-                    rawData: data
-                };
-                cb(st, st.BarometerCalibration.info[2], err, CObj);
+                st._barometricPressureCalibrationData = data;
+                cb(st, st.BarometerCalibration.info[2], err, data);
             });
         });
     },
@@ -309,11 +315,33 @@ SensorTag.prototype = {
 
         st.BarometerData.ch.read(function(err, data) {
 
-            var BObj = {
-                rawData: data
-            };
+            // For computation refer :  http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Barometric_Pressure_Sensor_2
+            var temp, // Temperature raw value from sensor
+                pressure, // Pressure raw value from sensor
+                S, // Interim value in calculation
+                O, // Interim value in calculation
+                p_a, // Pressure actual value in unit Pascal.
 
-            cb(st, st.BarometerData.info[2], err, BObj);
+                c0 = st._barometricPressureCalibrationData.readUInt16LE(0),
+                c1 = st._barometricPressureCalibrationData.readUInt16LE(2),
+                c2 = st._barometricPressureCalibrationData.readUInt16LE(4),
+                c3 = st._barometricPressureCalibrationData.readUInt16LE(6),
+
+                c4 = st._barometricPressureCalibrationData.readInt16LE(8),
+                c5 = st._barometricPressureCalibrationData.readInt16LE(10),
+                c6 = st._barometricPressureCalibrationData.readInt16LE(12),
+                c7 = st._barometricPressureCalibrationData.readInt16LE(14);
+
+            temp = data.readInt16LE(0);
+            pressure = data.readUInt16LE(2);
+
+            S = c2 + ((c3 * temp) / 131072.0) + ((c4 * (temp * temp)) / 17179869184.0);
+            O = (c5 * 16384.0) + (((c6 * temp) / 8)) + ((c7 * (temp * temp)) / 524288.0);
+            Pa = (((S * pressure) + O) / 16384.0);
+
+            Pa /= 100.0;
+
+            cb(st, st.BarometerData.info[2], err, Pa);
         });
     }
 
